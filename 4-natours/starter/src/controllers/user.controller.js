d2ea7c44f761ca08686003dfa771f12530009c26
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2019-07-06 20:33:34 -0300

    Split app into routes, controllers and middlewares.

---
+++ 4-natours/starter/src/controllers/user.controller.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    error: 'Undefined route'
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    error: 'Undefined route'
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    error: 'Undefined route'
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    error: 'Undefined route'
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    error: 'Undefined route'
+  });
+};
