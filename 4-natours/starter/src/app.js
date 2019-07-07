353aef500d97c2996a9b5c3ddfbe65960132ed65
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2019-07-06 21:13:37 -0300

    Add environment variables support.

--- 4-natours/starter/src/app.js
+++ 4-natours/starter/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
 
 // Middlewares.
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
@@ -12,6 +15,7 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 const API_VERSION = '/api/v1';
 const toursRouter = require('./routes/tour.routes');
 const usersRouter = require('./routes/user.routes');
+
 app.use(`${API_VERSION}/tours`, toursRouter);
 app.use(`${API_VERSION}/users`, usersRouter);
 
--- 4-natours/starter/src/server.js
+++ 4-natours/starter/src/server.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Set ENV variables before exec app.
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
 const app = require('./app');
 
 // Server.
-const port = 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listen on port: ', port));
